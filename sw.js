8bbe706e317aab422ff455c3b83c3f204eb3f66f
geoffroy-hub <abotchigeoffroy1@gmail.com>
2026-03-20 18:17:03 +0000

    Add files via upload

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
    Offline : redirection automatique vers offline.html
    ============================================================ */
 
-const CACHE_V = 'Miralocks-v10'; // Cache-busting forcé pour iPhone (Date Picker Fix)
+const CACHE_V = 'Miralocks-v13'; // Calendrier VISIBILITÉ FIX v2
 const STATIC = [
   '/offline.html',
   '/css/styles.css',
